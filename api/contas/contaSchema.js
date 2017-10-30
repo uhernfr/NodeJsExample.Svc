e6f9bc6439b449b0db815b159f1adbae9e31c543
Francisco Hernando Coelho da Silva <francisco.coelho@linx.com.br>
2017-10-30 09:18:58 -0200

    Mudança tipo dado Saldo

--- api/contas/contaSchema.js
+++ api/contas/contaSchema.js
@@ -5,7 +5,7 @@ var contaSchema = new Schema({
       descricao: { type: String, required: true },
       cor: { type: String, required: true },
       tipoConta: { type: String, required: true },
-      saldo: { type: String, required: true }
+      saldo: { type: Number, required: true }
 });
 console.log("schema")
 module.exports = mongoose.model('contas', contaSchema);
